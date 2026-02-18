a9cbf4619d9f7d7b4b75c0bf4fceb6ff3c3ab950
eli358-roma <elisa01.romagnoli@studenti.unicam.it>
2026-02-18 16:50:32 +0100

    Enable scheduling in HackHubApplication

--- Romagnoli_HackHub/src/main/java/com/hackhub/HackHubApplication.java
+++ Romagnoli_HackHub/src/main/java/com/hackhub/HackHubApplication.java
@@ -2,8 +2,11 @@ package com.hackhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
+@EnableScheduling
 public class HackHubApplication {
 
     public static void main(String[] args) {
